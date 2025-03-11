f223e1f761fd70cba768462bf82f153a8f07661c
arco <1921141189@qq.com>
2025-03-12 00:21:14 +0800

    【新增】增加了构建出使用busybox作为initrd阶段init程序的脚本，可以在不挂载根文件系统的情况下调试

---
+++ arco-virt/make-initrd-busybox.sh
@@ -0,0 +1,32 @@
+# clean initrd
+rm -rf initrd
+mkdir initrd
+# copy toolchains-sysroot
+tar -xvf repo/sysroot.tar.gz -C initrd
+rm -rf initrd/usr
+rm -rf initrd/data/*
+# make essential dir
+
+# copy executable to bin
+cp -r repo/bin/* initrd/bin
+# copy driver file
+# (consider the of compatible of kernel, do not copy driver file
+
+# copy init file
+cp repo/bin/busybox initrd/init
+# create symlink of busybox
+cp repo/script/createlink.sh initrd
+# copy ko
+cp nvme.ko initrd
+# create busybox link
+cd initrd
+sh createlink.sh
+cd ..
+
+# make cpio.gz
+echo "find .|cpio -H newc -o|gzip -c > ../initrd.cpio.gz" > initrd/makecpiogz
+chmod +x initrd/makecpiogz
+cd initrd
+sh makecpiogz
+cd ..
+
